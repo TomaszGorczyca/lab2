e99eef8c7a9f2e951b272cc6fea3aed80733c820
TomaszGorczyca <164004387+TomaszGorczyca@users.noreply.github.com>
2025-02-12 05:48:33 +0000

    Update ex2.js

--- ex2.js
+++ ex2.js
@@ -3,19 +3,19 @@ var myTodos = /** @class */ (function () {
         this.myTasks = [];
     }
     myTodos.prototype.addTask = function (task) {
-        var lenght = this.myTasks.push(task);
+        var lenght = this.myTasks.push(task);// adds the task
         console.log(task + " added to my tasks");
-        return lenght;
+        return lenght; // returns updated task list
     };
     myTodos.prototype.listAlltasks = function () {
         this.myTasks.forEach(function (Element) {
-            console.log(Element + " is in my Tasks List.");
+            console.log(Element + " is in my Tasks List."); // prints each task in the list
         });
     };
     myTodos.prototype.deleteTasks = function (task) {
-        var index = this.myTasks.indexOf(task, 0);
-        if (index != -1) {
-            this.myTasks.splice(index, 1);
+        var index = this.myTasks.indexOf(task, 0); //finds the index of the task
+        if (index != -1) {// if task exits in the list
+            this.myTasks.splice(index, 1);//removes the task from the array
             console.log(task + " remove from my tasks list.");
         }
         else {
@@ -23,7 +23,7 @@ var myTodos = /** @class */ (function () {
         }
         return this.myTasks.length;
     };
-    return myTodos;
+    return myTodos; // returns updated task list 
 }());
 var myTask = new myTodos();
 myTask.addTask("work");
